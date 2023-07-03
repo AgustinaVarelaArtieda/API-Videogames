d71ff996cf69a4dc4238cf4e0790f6ffcdc05bac
agusv <agusvarela5@gmail.com>
2023-07-03 18:43:19 -0300

    Base de datos. Modelos y conexiones creadas

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,40 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey:true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    platforms: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    releaseDate:  {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    // createdInDb: {      //para videojuegos creados en la base de datos
+    //   type: DataTypes.BOOLEAN,
+    //   allowNull: false,
+    //   defaultValue: true
+    // }
+  }, {timestamps:false});
 };
