aadba13a90c4e77bf066cb855fa3a6688cb44f8c
agusv <agusvarela5@gmail.com>
2023-07-06 23:41:57 -0300

    BackEnd. Controllers. Terminados controllers para GET, hay errores en el POST.

--- api/src/controllers/Videogames/newVideogame.js
+++ api/src/controllers/Videogames/newVideogame.js
@@ -1,18 +1,32 @@
 const {Videogame,Genres}=require('../../db')  //traigo los modelos desde la BASEDEDATOS
 
+const {Op}=require('sequelize')
 
-const createVideogame= async(name,image,description,platforms,releaseDate,rating,genre)=>{        //recibo por parametro lo que se ingresa en el form
+const createVideogame= async(data)=>{        //recibo por parametro lo que se ingresa en el form
+    const {name,image,description,platforms,releaseDate,rating,genres}=data
 
-    if(![name,image,description,platforms,releaseDate,rating].every(Boolean)){  //esto verifica que toda la info requerida sea subida
-        return 'faltan datos'
-    }else{
-        //para crear un nuevo juego
-        const newGame= await Videogame.create({name,image,description,platforms,releaseDate,rating})
+    const newVideogame=await Videogame.create({
+        name,
+        image,
+        description,
+        platforms,
+        releaseDate,
+        rating,
+        genres
+    })
+    
+    // if(!name||!image||!description||!platforms||!releaseDate||!rating){  //esto verifica que toda la info requerida sea subida
+    //     return 'faltan datos'
+    // }
 
-        await newGame.addGenres(genre)    //para agregar el genero del juego(que viene del modelo GENRES)
-        
-        return newGame
-    }
+    let genresDB= await Genres.findAll({    //para agregar el genero del juego(que viene del modelo GENRES)
+       where:{name:genre}
+   })
+
+  newVideogame.addGenres(genresDB) //agrego un nuevo genres a la DB
+
+  return newVideogame
+    // }
 }
 
 module.exports=createVideogame
