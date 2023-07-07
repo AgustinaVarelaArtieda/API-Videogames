4d62ebdf8f6c765fad1b34221dee668236199a35
agusv <agusvarela5@gmail.com>
2023-07-06 23:39:48 -0300

    BackEnd. Controllers. getGenres: guarda los datos en la DB y retorna un array con los nombres.

--- api/src/controllers/Genres/genresFromApi.js
+++ api/src/controllers/Genres/genresFromApi.js
@@ -1,7 +1,30 @@
-const axios=require('axios')
+const axios=require('axios');
 
-const genre_API=()=>{   //traigo los generos de la API
+const {Genres} = require('../../db');
 
+const {API_KEY}=process.env
+
+const url=`https://api.rawg.io/api/genres?key=${API_KEY}`
+
+const genre_API=async()=>{   
+    
+    const genresExistingInDB= await Genres.findAll()        //Busco y traigo los generos que existen en mi DB
+
+    const namesGenres=genresExistingInDB.map(el=>el.name);  //Extraigo solo los nombres de los generos y los guardo en un ARRAY
+
+    if(!genresExistingInDB.length){     //si mi DB esta vacia
+        
+        const genresAPI=await axios.get(url)
+
+        const {data:{results}}=genresAPI
+    
+        await results.map(el=>{
+            Genres.create({name:el.name});          //aqui agrego esos generos a la DB
+            namesGenres.push(el.name);       //pusheo los nombres de los generos al ARRAY que retorno
+        });
+    }
+
+    return namesGenres
 }
 
 module.exports=genre_API
