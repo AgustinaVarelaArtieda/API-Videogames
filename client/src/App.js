a3c6aebf5f29446f022eb5bd9af2c8cbceef6b04
agusv <agusvarela5@gmail.com>
2023-07-08 22:08:24 -0300

    FrontEnd. Comenzando.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
+import {Route, Switch} from 'react-router-dom'
+
+import landingPage from './components/Landing Page/landingPage';
+import { Home } from './components/Home Page/homePage';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={landingPage}/>
+          <Route path='/home' component={Home}/>
+        </Switch>
+      </div>
   );
 }
 
