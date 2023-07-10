3ae1b54bf0f89a3f2c1cd0c3d74e4bd65d439077
agusv <agusvarela5@gmail.com>
2023-07-09 22:23:25 -0300

    Creacion basica de landingPage. Despues fijarse en css y modificar

--- client/src/components/Landing Page/landingPage.jsx
+++ client/src/components/Landing Page/landingPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 
-export default function landingPage(){
+export default function LandingPage(){
     return(
         <div>
             <h1>Welcome to the Landing Page</h1>
