b22497afcffb6bc5c6baffb4896af818ab7ed2d7
agusv <agusvarela5@gmail.com>
2023-07-09 22:22:18 -0300

    Realice todo en homePage, despues pasar las funciones y renderizados a sus correspondientes componentes

--- client/src/components/Home Page/homePage.jsx
+++ client/src/components/Home Page/homePage.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { Link } from "react-router-dom/cjs/react-router-dom";
+import { Link } from "react-router-dom";
 
-import { getVideogames } from "../../redux/actions";
+import { getVideogames, filterGamesByGenre, filterOrigin, orderByName } from '../../redux/actions'
 
 import Card from '../Card/card'
+import Pagination from '../Pagination/pagination'
 
-export function Home (){
-
+export default function Home (){
+    //'Traigo' el estado global
     const dispatch = useDispatch();
-    const allVideogames = useSelector((state)=>state.videogames);       //Trae todo lo que esta en el estado de videogames
+    const allVideogames = useSelector((state)=>state.videogames);       //Trae todo lo que esta en el estado de videogames(ESTADO GLOBAL)
+
+    //PAGINACION
+    //Creacion de estados LOCALES
+    const [currentPage, setCurrentPage]=useState(1)     //Pagina actual y seteo de pagina actual, empieza en 1 porque siempre voy a entrar a la primera pagina
+    const [gamesPerPage, setGamesPerPage]=useState(15)  //Cuantos games por pagina quiero
+    //Creacion de constantes para ubicar los juegos segun el indice
+    const indexOfLastGame=currentPage*gamesPerPage  
+    const indexOfFirstGame=indexOfLastGame-gamesPerPage
+    //Division de games por pagina
+    const currentGames= allVideogames.slice(indexOfFirstGame,indexOfLastGame) //Esta const contiene los personajes que van a tener la pagina actual
+    //Funcion para el renderizado del paginado
+    function paginated(pageNumber){
+        setCurrentPage(pageNumber)
+    }
+
 
     //Traigo los videojuegos del estado cuando se monte el HOME
     useEffect(()=>{
@@ -24,10 +40,38 @@ export function Home (){
         dispatch(getVideogames());
     }
 
+
+    //FILTRADO
+    //Filtrado por genero
+    function handleFilterGenre(e){
+        const genre=e.target.value;
+        dispatch(filterGamesByGenre(genre));
+    }
+    //filtrado por origen(API,DB)
+    function handleFilterOrigin(e){
+        const origin=e.target.value;
+        dispatch(filterOrigin(origin));
+    }
+    //filtrado por
+
+    //ORDEN
+    //genero un estado local
+    const [nameOrder,setNameOrder]=useState('');
+    //ordenar por nombre
+    function handleSortName(e){
+        const nameOrder=e.target.value
+        e.preventDefault();
+        dispatch(orderByName(nameOrder))    
+        setCurrentPage(1);      //para que empiece a ordenar desde la primera pagina
+        setNameOrder(`Ordenado ${nameOrder}`)   //me modifica el estado local y me renderiza la pagina ordenada.
+    }
+    //ordenar por rating
+
+
     return(
         <div>
-            <Link to='/videogame'>Crear videojuegpo</Link>
             <h1>VIDEOJUEGOS</h1>
+            <Link to='/videogames'>Crear videojuego</Link>
 
             {/*Esto de abajo convendria ponerlo en la NAVBAR*/}
             <button onClick={e=>{handleClick(e)}}>
@@ -38,17 +82,17 @@ export function Home (){
             {/*Esto de abajo convendria ponerlo en la SEARCHBAR*/}
             <div>
                 {/*Esto de abajo es para los filtros*/}
-                <select>
+                <select onChange={e=>handleSortName(e)}>
                     <option value='asc'>Acsendente</option>
                     <option value='desc'>Descendente</option>
                 </select>
-                <select>
+                <select onChange={e=>handleFilterGenre(e)}>
                     {/*Esto se puede hacer con un map para pasar todos los generos existentes*/}
                     <option value='All'>Todos</option>
                     <option value='aca se le pasa el mismo valor que tiene en la API'>Action</option>
-                    <option value='adv'>Adventure,etccc</option>
+                    <option value='Adventure'>Adventure,etccc</option>
                 </select>
-                <select>
+                <select onChange={e=>handleFilterOrigin(e)}>
                     <option value='All'>Todos</option>
                     <option value='API'>Datos de la API(existentes)</option>
                     <option value='DB'>Datos de la DB</option>
@@ -57,13 +101,17 @@ export function Home (){
             </div>
             {/*Esto de arriba convendria ponerlo en la SEARCHBAR*/}
             
+            {/*Esto trae las cards al home*/}
             {
-                allVideogames?.map(el=>{
+                currentGames?.map(el=>{
                     return(
                         <Card name={el.name} image={el.image} genre={el.genre}/>
                     )
                 })
             }
+            {/*Esto realiza el paginado*/}
+            <Pagination gamesPerPage={gamesPerPage} allVideogames={allVideogames.length} paginated={paginated}/>
+        
         </div>
     )
 }
