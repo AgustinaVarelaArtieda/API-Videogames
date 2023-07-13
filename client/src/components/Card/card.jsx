c9879d6ef55aea5a034a8f274072e367e9dcbcb7
agusv <agusvarela5@gmail.com>
2023-07-12 21:20:18 -0300

    Funcionamiento. Problemas con el link del detalle. Faltan estilos.

--- client/src/components/Card/card.jsx
+++ client/src/components/Card/card.jsx
@@ -1,11 +1,23 @@
 import React from "react";  
 
-export default function Card({name, image, genre}){
+import { useNavigate, useParams } from "react-router-dom";
+
+export default function Card({name, image, genres}){
+    const navigate=useNavigate()
+    const { key }=useParams()
+
+    const handleClick = (e) => {
+
+        navigate(`/home/${key}`)
+    }
+
     return(
         <div>
             <h3>{name}</h3>
-            <img src={image} alt={name} />
-            <p>{genre}</p>
+           
+            <img src={image} alt={name} onClick={(e)=>handleClick(e)}/>
+            
+            <h4>{genres}</h4>
         </div>
     )
 }
