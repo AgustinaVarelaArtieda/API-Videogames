4d09d787f152860b8657cd0671ca39db602a8cc5
agusv <agusvarela5@gmail.com>
2023-07-23 23:31:26 -0300

    falta agregar css

--- client/src/components/Landing Page/landingPage.jsx
+++ client/src/components/Landing Page/landingPage.jsx
@@ -4,9 +4,10 @@ import {Link} from 'react-router-dom'
 export default function LandingPage(){
     return(
         <div>
-            <h1>Welcome to the Landing Page</h1>
+            <h1>Welcome to the Videogames Page</h1>
+
             <Link to='/home'>
-                <button>START</button>
+                <button>PRESS START</button>
             </Link>
         </div>
     )
