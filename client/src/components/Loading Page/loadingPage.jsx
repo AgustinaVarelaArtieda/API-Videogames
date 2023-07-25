993f62283914bb2150a3be5ca8998ab04cf4e327
agusv <agusvarela5@gmail.com>
2023-07-24 22:38:47 -0300

    Creacion de pagina de carga

---
+++ client/src/components/Loading Page/loadingPage.jsx
@@ -0,0 +1,7 @@
+import style from '../Loading Page/loadingPage.module.css'
+
+export default function Loading(){
+    return(
+        <div className={style.loader}><h1>LOADING ↻</h1></div>
+    )
+}
