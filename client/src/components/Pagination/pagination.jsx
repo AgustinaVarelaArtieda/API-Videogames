6aa6993a7359f430da32902af7ee58fc465c613a
agusv <agusvarela5@gmail.com>
2023-07-29 16:06:32 -0300

    Numero de pagina actual diferenciado.

--- client/src/components/Pagination/pagination.jsx
+++ client/src/components/Pagination/pagination.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import style from '../Pagination/pagination.module.css'
 
 //Este componente renderiza los numeritos en si
-export default function Pagination({gamesPerPage, allVideogames, paginated}){
+export default function Pagination({gamesPerPage, allVideogames, paginated, currentPage}){
     const pageNumber=[]
 
     for(let i=1;i<=Math.ceil(allVideogames/gamesPerPage);i++){
@@ -12,9 +12,9 @@ export default function Pagination({gamesPerPage, allVideogames, paginated}){
 
     return(
         <nav className={style.page}>
-            <ul className='pagination'>
-                {pageNumber?.map(number=>(
-                    <li className='number' key={number}>
+            <ul className={`pagination ${style.pagination}`}>
+        {pageNumber?.map((number) => (
+          <li className={`${style.number} ${currentPage === number ? style.active : ''}`} key={number}>
                         <button className={style.btn} onClick={()=>paginated(number)}>{number}</button>
                     </li>
                 ))}
